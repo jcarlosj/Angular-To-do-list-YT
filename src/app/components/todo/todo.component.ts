35b70674560ad5d6786e7e3c39898b01efce9177
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-05-05 10:44:04 -0500

    [+] todo.component.ts: Atributos

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { Todo } from './../../models/Todo';
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent {
-
+  title: String = 'Todo';
+  todos: Array<Todo> = [];
 }
